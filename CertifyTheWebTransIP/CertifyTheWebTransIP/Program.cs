c89958c848af1f43d6aa3bdd97db74f1bda594a9
Timo Hutjens <timo.microsoft@iq-design.nl>
2018-08-13 17:08:29 +0200

    Implemented base logic

---
+++ CertifyTheWebTransIP/CertifyTheWebTransIP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using TransIp.Api;
+using TransIp.Api.Dto;
+
+namespace CertifyTheWebTransIP
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var domainService = new DomainService(ConfigurationManager.AppSettings["UserName"], ClientMode.ReadWrite, ConfigurationManager.AppSettings["PrivateKey"]);
+
+            var domain = await domainService.GetInfoAsync(args[0]);
+
+            // Get the existing dns entries
+            var entries = domain.DnsEntries.ToList();
+
+            // Create a new entry
+            entries.Add(new DnsEntry
+            {
+                Name = args[1],
+                Type = DnsEntryType.TXT,
+                Expire = 3600, // 1 hour
+                Content = args[2]
+            });
+
+            // Save
+            await domainService.SetDnsEntriesAsync(args[0], entries.ToArray());
+        }
+    }
+}
